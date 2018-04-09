8b36d4461553106d13337088d4da742e2a264c27
Simon Stelter <stelter@tzi.de>
2018-04-09 09:34:33 +0200

    added ring light switch service ros node

---
+++ scripts/ring_light_switch.py
@@ -0,0 +1,17 @@
+import rospy
+from std_srvs.srv import SetBool, SetBoolResponse
+import os
+
+def cb(srv):
+    if srv.data:
+        os.system('echo "9" | nc 192.168.102.114 3000')
+    else:
+        os.system('echo "0" | nc 192.168.102.114 3000')
+    r = SetBoolResponse()
+    r.success = True
+    r.message = 'muh'
+    return r
+
+rospy.init_node('ring_light_switch')
+s = rospy.Service('~/setbool', SetBool, cb)
+rospy.spin()
