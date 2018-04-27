6b4287f3ec243f00feda94d2b6f0aa60781170a7
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2018-04-27 14:20:36 +0200

    Added empty stub for new ground truth that has to be collected for evaluation.

---
+++ data/ground_truth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# TODO(Georg): add license
+import rospy
+
+
+def instantiate_ground_truth():
+    # TODO(Kevin): please complete me; feel free to add as many helper functions as you need
+    pass
+
+
+def visualize(data):
+    # TODO(Georg): please complete me
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        visualize(instantiate_ground_truth())
+    except rospy.ROSInterruptException:
+        pass
