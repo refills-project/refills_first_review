9ac542bfe03b518228245584b52013dfe2551b33
simon <stelter@tzi.de>
2018-06-18 14:08:15 +0200

    indent ringlight switch using spaces

--- scripts/ring_light_switch.py
+++ scripts/ring_light_switch.py
@@ -5,20 +5,20 @@ from std_srvs.srv import SetBool, SetBoolResponse
 import os
 
 def cb(srv):
-	msg = 'echo "{}" | nc 192.168.102.114 3000'
+    msg = 'echo "{}" | nc 192.168.102.114 3000'
     r = SetBoolResponse()
     if srv.data:
-    	msg = msg.format(9)
+        msg = msg.format(9)
     else:
-    	msg = msg.format(0)
+        msg = msg.format(0)
     for i in range(5):
-    	read = os.popen(msg).read()
-    	r.message = read	
-    	if 'Setting light to:' in read:
-    		r.success = True
-    		break
+        read = os.popen(msg).read()
+        r.message = read
+        if 'Setting light to:' in read:
+            r.success = True
+            break
     else:
-    	r.success = False
+        r.success = False
     return r
 
 rospy.init_node('ring_light_switch')
