3ecc4a56b2f15aeec8bf22b595bab9114d2b40d8
simon <stelter@tzi.de>
2018-06-18 14:20:27 +0200

    ringlight switch has internal state

--- scripts/ring_light_switch.py
+++ scripts/ring_light_switch.py
@@ -5,21 +5,43 @@ from std_srvs.srv import SetBool, SetBoolResponse
 import os
 
 def cb(srv):
-    msg = 'echo "{}" | nc 192.168.102.114 3000'
     r = SetBoolResponse()
+    next_state = 0
     if srv.data:
-        msg = msg.format(9)
+        next_state = 9
+    msg = msg.format(next_state)
+    expected_result = expected_result.format(echo_to_bit[next_state])
+    if ring_light_state is None or ring_light_state != next_state:
+        for i in range(number_of_retries):
+            read = os.popen(msg).read()
+            r.message = '{}; after {} tries'.format(read, i)
+            if expected_result == read:
+                r.success = True
+                ring_light_state = next_state
+                break
+        else:
+            r.success = False
     else:
-        msg = msg.format(0)
-    for i in range(5):
-        read = os.popen(msg).read()
-        r.message = read
-        if 'Setting light to:' in read:
-            r.success = True
-            break
-    else:
-        r.success = False
+        r.message = 'ring light is already in the requested state.'
+        r.success = True
     return r
+
+echo_to_bit = {
+    0: 0,
+    1: 29,
+    2: 58
+    3: 87,
+    4: 116,
+    5: 145,
+    6: 174,
+    7: 203,
+    8: 232,
+    9: 255
+    }
+msg = 'echo "{}" | nc 192.168.102.114 3000'
+expected_result = 'Setting light to: {}\n'
+ring_light_state = None
+number_of_retries = 5
 
 rospy.init_node('ring_light_switch')
 s = rospy.Service('~setbool', SetBool, cb)
