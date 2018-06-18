cc1f9c2c2e04ef0969b0e5be28c63367c0166a84
simon <stelter@tzi.de>
2018-06-18 14:41:00 +0200

    added lock to ring light switch

--- scripts/ring_light_switch.py
+++ scripts/ring_light_switch.py
@@ -3,29 +3,31 @@
 import rospy
 from std_srvs.srv import SetBool, SetBoolResponse
 import os
+from multiprocessing import Lock
 
 def cb(srv):
-    global msg_template, echo_to_bit, expected_result_template, ring_light_state, number_of_retries
-    r = SetBoolResponse()
-    next_state = 0
-    if srv.data:
-        next_state = 9
-    msg = msg_template.format(next_state)
-    expected_result = expected_result_template.format(echo_to_bit[next_state])
-    if ring_light_state is None or ring_light_state != next_state:
-        for i in range(number_of_retries):
-            read = os.popen(msg).read()
-            r.message = '{}; after {} tries'.format(read, i+1)
-            if expected_result == read:
-                r.success = True
-                ring_light_state = next_state
-                break
+    global msg_template, echo_to_bit, expected_result_template, ring_light_state, number_of_retries, ring_light_lock
+    with ring_light_lock:
+        r = SetBoolResponse()
+        next_state = 0
+        if srv.data:
+            next_state = 9
+        msg = msg_template.format(next_state)
+        expected_result = expected_result_template.format(echo_to_bit[next_state])
+        if ring_light_state is None or ring_light_state != next_state:
+            for i in range(number_of_retries):
+                read = os.popen(msg).read()
+                r.message = '{}; after {} tries'.format(read, i+1)
+                if expected_result == read:
+                    r.success = True
+                    ring_light_state = next_state
+                    break
+            else:
+                r.success = False
         else:
-            r.success = False
-    else:
-        r.message = 'ring light is already in the requested state.'
-        r.success = True
-    return r
+            r.message = 'ring light is already in the requested state.'
+            r.success = True
+        return r
 
 echo_to_bit = {
     0: 0,
@@ -39,6 +41,7 @@ echo_to_bit = {
     8: 232,
     9: 255
     }
+ring_light_lock = Lock()
 msg_template = 'echo "{}" | nc 192.168.102.114 3000'
 expected_result_template = 'Setting light to: {}\n'
 ring_light_state = None
